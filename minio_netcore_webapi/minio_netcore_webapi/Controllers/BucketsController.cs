a716a4ad62135ad155d0a5a029155975ab9936af
msekinci <mehmetserkanekinci@gmail.com>
2023-07-18 01:57:58 +0300

    feat: bucket controller added

--- minio_netcore_webapi/minio_netcore_webapi/Controllers/BucketsController.cs
+++ minio_netcore_webapi/minio_netcore_webapi/Controllers/BucketsController.cs
@@ -1,15 +1,53 @@
-
 using Microsoft.AspNetCore.Mvc;
+using minio_netcore_webapi.Models;
+using minio_netcore_webapi.Services;
 
 namespace minio_netcore_webapi.Controllers;
 
 [ApiController]
-public class BucketsController : ControllerBase
+[Route("api/[controller]/[action]")]
+public class BucketsController : BaseController
 {
-    // GET
-    public IActionResult GetBucketList()
+    private readonly BucketService _bucketService;
+
+    public BucketsController(BucketService bucketService)
     {
-        
-        return Ok();
+        _bucketService = bucketService;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> MakeBucket([FromBody] MakeBucketDto model)
+    {
+        return CreateActionResult(await _bucketService.MakeBucket(model.BucketName));
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> MakeBucketWithLock([FromBody] MakeBucketDto model)
+    {
+        return CreateActionResult(await _bucketService.MakeBucketWithLock(model.BucketName));
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> ListBuckets()
+    {
+        return CreateActionResult(await _bucketService.ListBuckets());
+    }
+    
+    [HttpGet("{bucketName}")]
+    public async Task<IActionResult> BucketExist(string bucketName)
+    {
+        return CreateActionResult(await _bucketService.BucketExist(bucketName));
+    }
+    
+    [HttpDelete("{bucketName}")]
+    public async Task<IActionResult> RemoveBucket(string bucketName)
+    {
+        return CreateActionResult(await _bucketService.RemoveBucket(bucketName));
+    }
+    
+    [HttpGet("{bucketName}")]
+    public async Task<IActionResult> ListObjects(string bucketName)
+    {
+        return CreateActionResult(await _bucketService.ListObjects(bucketName));
     }
 }
---
+++ minio_netcore_webapi/minio_netcore_webapi/Models/MakeBucketDto.cs
@@ -0,0 +1,6 @@
+namespace minio_netcore_webapi.Models;
+
+public class MakeBucketDto
+{
+    public string BucketName { get; set; } = null!;
+}
